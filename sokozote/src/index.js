fa24096f097c91162bdc25fa307c159a5507b68c
joseph muchene <66246216+joseph-muchene@users.noreply.github.com>
2022-09-30 22:35:07 +0300

    configured page routing

--- sokozote/src/index.js
+++ sokozote/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./Router";
+import reportWebVitals from "./reportWebVitals";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
